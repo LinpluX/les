b459a27ffdf28255ca2293f1117afb16470ae649
LinpluX <podtelkovs@bk.ru>
2021-11-10 00:15:10 +0800

    Находим остаток

---
+++ ex/Program.cs
@@ -0,0 +1,21 @@
+﻿Random random = new Random();
+ 
+//Получить случайное число (в диапазоне от 0 до 10)
+int value = random.Next(100, 999);
+Console.WriteLine($"Ваше число: {value}");
+string Number = Convert.ToString(value);
+string numPosition(string number,  int position)
+{
+    string result = string.Empty;
+    result = result + number[position];
+    return result;
+}
+int FN=Convert.ToInt32(numPosition(Number, 0));
+int TN=Convert.ToInt32(numPosition(Number, 2));
+Console.WriteLine($"Оно же только преобразованное: ");
+Console.Write(FN);
+Console.Write(TN);
+
+
+
+
---
+++ ex2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите делитель, который будет меньше первого числа : ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int Krat(int num1, int num2)
+{if (num1%num2==0)
+{
+    Console.WriteLine("Число делится без остатка:");
+    return num1%num2;
+}
+else
+{
+    Console.WriteLine("Число делится с остатком, остаток равен:");
+    return num1/num2;;
+}
+}
+Console.WriteLine(Krat(a,b));
